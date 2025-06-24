b8123bb92d1b86d42d08862d2f360b352556a9c6
lucasgordon <lucas.gordon@queensu.ca>
2025-06-23 23:13:35 -0400

    Venv

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,9 +3,13 @@
 # Exit on error
 set -o errexit
 
+python3 -m venv venv
+source venv/bin/activate
+
+pip install -r requirements.txt
+
 bundle install
 rm -rf node_modules package-lock.json
 npm install
-pip install -r requirements.txt
 bin/rails assets:precompile
 bin/rails assets:clean
